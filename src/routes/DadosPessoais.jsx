59c50f1517cca9f090fed634f7bc7d20003d0ab2
Lay Cipriano <laysa.altheman@lexp.com.br>
2024-03-20 22:34:18 -0300

    att app

--- src/routes/DadosPessoais.jsx
+++ src/routes/DadosPessoais.jsx
@@ -1,8 +1,34 @@
 import Menu from '../routes/Menu.jsx';
 import '../assets/css/DadosPessoais.css';
+import { useState } from 'react';
 
 
 function DadosPessoais() {
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [linkedin, setLinkedin] = useState('');
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        switch(name) {
+            case 'nome': setNome(value);
+                break;
+            case 'email': setEmail(value);
+                break;
+        }
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(`Nome: ${nome}`)
+        console.log(`Email: ${email}`)
+    }
+
     return (
         <main className='main'>
             <Menu/>
@@ -12,18 +38,24 @@ function DadosPessoais() {
                     <h3>Preencha as informações abaixo</h3>
                 </div>
 
-                <div className="form">
-                    <input className='inputForm' type="text" placeholder='Nome Completo' />
-                    <input className='inputForm' type="email" placeholder='Seu melhor e-mail' />
+                <form className="form" onSubmit={handleSubmit}>
+                    <input className='inputForm' type="text" 
+                    // value={nome} 
+                    onChange={handleChange} 
+                    placeholder='Nome Completo' />
+                    <input className='inputForm' type="email" 
+                    value={email} 
+                    onChange={handleChange}
+                    placeholder='Seu melhor e-mail' />
                     <input className='inputForm' type="text" placeholder='Seu telefone' />
+                    <input className='inputForm' type="text" placeholder='Cargo almejado' />
                     <input className='inputForm' type="text" placeholder='Seu endereço' />
                     <input className='inputForm' type="text" placeholder='Seu LinkedIn' />
-                    <input className='inputForm' type="text" placeholder='Cargo almejado' />
 
                     <button className='inputForm btnProximo' type='Submit'>
                         próximo
                     </button>
-                </div>
+                </form>
             </div>
         </main>
 
